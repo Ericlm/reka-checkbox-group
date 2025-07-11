b7991093d965f9c48a454f3e54db47ef0fb085f3
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-07-11 15:18:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-pink-200">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, similique.
+  </div>
+</template>
